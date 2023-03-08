a1ec5b51a619e040394a3f69dee423e75e285732
Roy Huaman <RHavila789@gmail.com>
2023-03-07 20:19:08 -0500

    feat: fix modal show

--- src/components/WorkItem/WorkItem.tsx
+++ src/components/WorkItem/WorkItem.tsx
@@ -17,23 +17,25 @@ const WorkItem: React.FC<WorkItemProps> = (work) => {
 	const { id, img, name, tags, animationDelay } = work;
 
 	return (
-		<div className="col-12 col-lg-4 workitem" style={{ animationDelay: `${animationDelay}s` }}>
-			<div className="border rounded">
-				<div className="work-container-img rounded-top bg-white" data-bs-toggle="modal" data-bs-target={`#${id}modal`}>
-					<img className="work-image" src={img} alt="work-image" />
-					<button className="border-0 work-container-interactive">
-						<div className="px-2 py-1 rounded bg-primary">View more</div>
-					</button>
-				</div>
-				<div className="py-3 px-3">
-					<h4>{name}</h4>
-					<Tags tags={tags} />
+		<>
+			<div className="col-12 col-lg-4 workitem" style={{ animationDelay: `${animationDelay}s` }}>
+				<div className="border rounded">
+					<div className="work-container-img rounded-top bg-white" data-bs-toggle="modal" data-bs-target={`#${id}modal`}>
+						<img className="work-image" src={img} alt="work-image" />
+						<button className="border-0 work-container-interactive">
+							<div className="px-2 py-1 rounded bg-primary">View more</div>
+						</button>
+					</div>
+					<div className="py-3 px-3">
+						<h4>{name}</h4>
+						<Tags tags={tags} />
+					</div>
 				</div>
 			</div>
 			<Suspense fallback={<p>Loading...</p>}>
 				<ModalWork {...work} />
 			</Suspense>
-		</div>
+		</>
 	);
 };
 
