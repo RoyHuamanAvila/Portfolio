637db1e833d19aa5d70929f88e1eadf929ebea19
Roy Huaman <RHavila789@gmail.com>
2023-09-16 20:25:36 -0500

    feat: change to ES

--- src/components/Experience/ExperienceView.tsx
+++ src/components/Experience/ExperienceView.tsx
@@ -1,36 +1,39 @@
-import { Tags } from "../Tags";
+import './Experience.scss'
 
 const ExperienceView = () => {
 
     return (
         <div id="Experience" className="row py-5 mb-5">
             <div className="col-12 col-lg-6">
-                <p className="blockquote-footer">CAREER PATH</p>
-                <h3>Work Experiences</h3>
+                <p className="blockquote-footer">RUTA DE CARRERA</p>
+                <h2>Experiencia</h2>
                 <div className="pt-5 pb-2">
                     <button className="text-primary border-0 bg-transparent ps-0">Make It Real Camp</button>
                 </div>
             </div>
             <div className="col-12 col-lg-6">
-                <h5>FullStack Developer</h5>
+                <h5>Desarrollador Fullstack</h5>
                 <p>Colombia</p>
                 <p>Jul 2022 - Nov 2022 - Full-time</p>
-                <Tags tags={['React', 'Sass', 'Nodejs', 'MongoDB']} />
+                <div className="d-flex gap-2">
+                    {
+                        ['React', 'Sass', 'Nodejs', 'MongoDB'].map((tag, index) => <span className="tag" key={index}>{tag}</span>)
+                    }
+                </div>
                 <hr />
                 <p className="fw-semibold">TuVideo (Video Platform)</p>
                 <ul className="list-group">
                     <li className="list-group-item border-0">
-                        In charge of the User/Channel model and the endpoints to which it is necessary to create,
-                        edit it. (Frontend/Backend)
+                        Encargado del modelo de Usuario/Canal y el CRUD del usuario. (Frontend/Backend)
                     </li>
                     <li className="list-group-item border-0">
-                        In charge of the total editing section of the Channel. (Frontend/Backend)
+                        A cargo de la sección de edición total del Canal. (Frontend/Backend)
                     </li>
                     <li className="list-group-item border-0">
-                        Responsible for the administration of the Cloudinary service
+                        Responsable de la administración del servicio Cloudinary.
                     </li>
                     <li className="list-group-item border-0">
-                        Support in the comments section (Backend)
+                        Soporte en la sección de comentarios (Backend)
                     </li>
                 </ul>
             </div>
