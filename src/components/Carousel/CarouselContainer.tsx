49fe0429a3bbfdd7c14b272167d157069d925869
Roy Huaman <RHavila789@gmail.com>
2023-09-23 13:26:38 -0500

    feat: carousel component desktop design

--- src/components/Carousel/CarouselContainer.tsx
+++ src/components/Carousel/CarouselContainer.tsx
@@ -1,8 +1,74 @@
-import { type FC } from 'react'
+import { ReactNode, type FC, useRef, useEffect, useState } from 'react'
 import CarouselView from './CarouselView'
 
-const CarouselContainer: FC = () => {
-  return <CarouselView />
+interface CarouselContainerProps {
+	children: ReactNode[]
+	maxItemsPerScroll: number
+	minWidth: number
+}
+
+const CarouselContainer: FC<CarouselContainerProps> = ({
+	children,
+	maxItemsPerScroll,
+	minWidth,
+}) => {
+	const carouselListRef = useRef<HTMLUListElement>(null)
+	const [translate, setTranslate] = useState(0)
+	const [widthItem, setWidthItem] = useState(0)
+	const [itemsPerScroll, setItemsPerScroll] = useState(maxItemsPerScroll)
+
+	useEffect(() => {
+		window.addEventListener('resize', updateWidthItem)
+
+		updateWidthItem()
+
+		return () => {
+			window.removeEventListener('resize', updateWidthItem)
+		}
+	}, [])
+
+	const updateWidthItem = () => {
+		if (carouselListRef.current) {
+			const containerCarousel = carouselListRef.current.parentElement
+
+			if (containerCarousel) {
+				if (containerCarousel.offsetWidth > minWidth) {
+					setWidthItem(containerCarousel?.offsetWidth / itemsPerScroll)
+				} else {
+					setWidthItem(containerCarousel.offsetWidth)
+				}
+			}
+		}
+		setTranslate(0)
+	}
+
+	const handleRightArrow = () => {
+		if (carouselListRef.current) {
+			const canMove =
+				(Math.abs(translate) + widthItem) * itemsPerScroll <
+				carouselListRef.current.offsetWidth
+			if (canMove) setTranslate(translate - widthItem)
+		}
+	}
+
+	const handleLeftArrow = () => {
+		if (carouselListRef.current) {
+			const canMove = translate !== 0
+			if (canMove) setTranslate(translate + widthItem)
+		}
+	}
+
+	return (
+		<CarouselView
+			carouselListRef={carouselListRef}
+			widthItem={widthItem}
+			handleRightArrow={handleRightArrow}
+			handleLeftArrow={handleLeftArrow}
+			translate={translate}
+		>
+			{children}
+		</CarouselView>
+	)
 }
 
 export default CarouselContainer
--- src/components/Carousel/CarouselView.tsx
+++ src/components/Carousel/CarouselView.tsx
@@ -1,8 +1,32 @@
-import { type FC } from "react"
+import { ReactNode, type FC, RefObject } from "react"
+import './Carousel.scss'
 
-const CarouselView: FC = () => {
+interface CarouselViewProps {
+  children: ReactNode[],
+  carouselListRef: RefObject<HTMLUListElement>,
+  widthItem: number,
+  handleRightArrow: () => void,
+  handleLeftArrow: () => void,
+  translate: number
+}
+
+const CarouselView: FC<CarouselViewProps> = ({ children, carouselListRef, widthItem, handleRightArrow, handleLeftArrow, translate }) => {
   return (
-    <div>CarouselView</div>
+    <div className="custom-carousel">
+      <button className="carousel__arrow" title='Anterior' onClick={handleLeftArrow}>
+        <i className="bi bi-caret-left-fill"></i>
+      </button>
+      <div className="carousel__container-list">
+        <ul className="list-items" style={{ transform: `translate(${translate}px)` }} ref={carouselListRef}>
+          {
+            children.map((child, index) => <li key={index} style={{ width: `${widthItem}px` }}>{child}</li>)
+          }
+        </ul>
+      </div>
+      <button className="carousel__arrow" title="Siguiente" onClick={handleRightArrow}>
+        <i className="bi bi-caret-right-fill"></i>
+      </button>
+    </div>
   )
 }
 
